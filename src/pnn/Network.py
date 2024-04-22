7cdb85844eaf131f003f9d4ded0869c90da91d10
Michel Bauer <michel.bauer@mainpost.de>
2024-04-22 16:26:13 +0200

    add project files

---
+++ src/pnn/Network.py
@@ -0,0 +1,5 @@
+class Network():
+    def __init__(self):
+
+        pass
+    pass
---
+++ src/pnn/tensor.py
@@ -0,0 +1,8 @@
+from shape import Shape
+
+
+class Tensor():
+    def __init__(self):
+        self.elements = []
+        self.deltas = []
+        self.shapes = []
