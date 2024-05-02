32d2503dc370030c744d77590af17b5bf4dfaff6
Dominik Petermann <dominik.petermann@gmx.net>
2024-05-02 09:50:26 +0200

    loss

--- src/pnn/layer/loss.py
+++ src/pnn/layer/loss.py
@@ -8,20 +8,24 @@ class LossLayer(Layer):
         self.loss = loss
 
     def forward(self, predictions: list[Tensor], targets: list[Tensor]):
-        return np.sum([self.loss(target=targets[i], prediction=predictions[i]) 
-                       for i in range(len(targets))])
+        # return np.sum([self.loss(target=targets[i], prediction=predictions[i]) 
+        #                for i in range(len(targets))])
+        lossl = []
+        for i in range(len(targets)):
+            lossl.append(self.loss(target=targets[i], prediction=predictions[i]))
+        return np.sum(lossl)
         
     def backward(self, predictions: list[Tensor], targets: list[Tensor]):
         for i in range(len(targets)):
             predictions[i].deltas = self.loss(target=targets[i], prediction=predictions[i], forward = False) 
                 
 
-def mean_squared_error(target: Tensor, prediction: Tensor, forward: bool = True) -> float:  
-    if forward:
-        return np.sum(np.divide(np.power(target.elements - prediction.elements, 2), 2))
-    else:  
-        return prediction.elements - target.elements
+def mean_squared_error(target: Tensor, prediction: Tensor) -> float:  
+    #if forward:
+    return np.sum(np.divide(np.power(target.elements - prediction.elements, 2), len(target.elements)))
+    # else:  
+    #     return prediction.elements - target.elements
 
 
-def cross_entropy(target: Tensor, prediction: Tensor):
+def cross_entropy(target: Tensor, prediction: Tensor, forward: bool = True):
     return - np.sum(target.elements * np.log(prediction.elements))
