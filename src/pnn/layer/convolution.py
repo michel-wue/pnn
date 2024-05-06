24a9a27daf83722a60c07b815506a9d0b1e7ce20
UHPDome <dominik.petermann@gmx.net>
2024-05-06 22:20:07 +0200

    started convolution

--- src/pnn/layer/convolution.py
+++ src/pnn/layer/convolution.py
@@ -1,3 +1,4 @@
+import numpy as np
 from ..shape import Shape
 from .layer import Layer
 from ..tensor import Tensor
@@ -20,13 +21,14 @@ class Conv2DLayer(Layer):
         self.dilation = dilation
         self.padding = padding
         self.in_shape = in_shape
-        self.filter = None
-
-    def _init_filter(self):
-        
+        self.weights = Tensor(np.random.rand(kernel_size[0], kernel_size[1], num_filters, in_shape[2]), None)
+        self.bias = Tensor(np.zeros(num_filters), None)
 
     def forward(self, in_tensors: list[Tensor], out_tensors: list[Tensor]):
-
+        for tensor in in_tensors:
+            for y in range(len(tensor.elements) - self.kernel_size[0] + 1):
+                for x in range(len(tensor.elements[0]) - self.kernel_size[1] + 1):
+                    channel_values = tensor.elements[y][x]
 
     def backward(self, out_tensors: list[Tensor], in_tensors: list[Tensor]):
         pass
